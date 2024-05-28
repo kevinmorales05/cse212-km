f21cea573a9524ad5f4948c61cc2731277f9564b
Kevin Daniel Morales Estrella <kdmorale@pichincha.com>
2024-05-28 00:31:07 -0500

    finish week 2 activity

--- week02/code/Priority.cs
+++ week02/code/Priority.cs
@@ -3,28 +3,74 @@
         // TODO Problem 2 - Write and run test cases and fix the code to match requirements
         // Example of creating and using the priority queue
         var priorityQueue = new PriorityQueue();
-        Console.WriteLine(priorityQueue);
+
 
         // Test Cases
 
         // Test 1
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: Add this values [Andres (Pri:2), Juan (Pri:3), Bernardo (Pri:6), Victor (Pri:1)]
+        // Expected Result: [Andres (Pri:2), Juan (Pri:3), Victor (Pri:1)]
         Console.WriteLine("Test 1");
+        priorityQueue.Enqueue("Andres", 2);
+        priorityQueue.Enqueue("Juan", 3);
+        priorityQueue.Enqueue("Bernardo", 6);
+        priorityQueue.Enqueue("Victor", 1);
+        Console.WriteLine("High priority");
+        Console.WriteLine("Before Dequeue");
+        Console.WriteLine(priorityQueue);
+        Console.WriteLine("------------------");
+        Console.WriteLine("Dequeued element");
+        Console.WriteLine(priorityQueue.Dequeue());
+        Console.WriteLine("------------------");
+        Console.WriteLine("Actual Queue");
+        Console.WriteLine(priorityQueue);
+        priorityQueue.Dequeue();
+
 
         // Defect(s) Found: 
 
         Console.WriteLine("---------");
-
-        // Test 2
-        // Scenario: 
-        // Expected Result: 
+var priorityQueue2 = new PriorityQueue();
+         // Test 2
+        // Scenario: Add this values [Camila (Pri:1), Juan (Pri:3), Bernardino (Pri:6), Henry (Pri:9)]
+        // Expected Result: [Camila (Pri:1), Juan (Pri:3), Bernardino (Pri:6)]
         Console.WriteLine("Test 2");
+        priorityQueue2.Enqueue("Camila", 1);
+        priorityQueue2.Enqueue("Juan", 3);
+        priorityQueue2.Enqueue("Bernardino", 6);
+        priorityQueue2.Enqueue("Henry", 9);
+        Console.WriteLine("High priority");
+        Console.WriteLine("Before Dequeue");
+        Console.WriteLine(priorityQueue2);
+        Console.WriteLine("------------------");
+        Console.WriteLine("Dequeued element");
+        Console.WriteLine(priorityQueue2.Dequeue());
+        Console.WriteLine("------------------");
+        Console.WriteLine("Actual Queue");
+        Console.WriteLine(priorityQueue2);
+        priorityQueue2.Dequeue();
 
-        // Defect(s) Found: 
-
+var priorityQueue3 = new PriorityQueue();
         Console.WriteLine("---------");
+        // Test 3
+        // Scenario: Add this values [Andy (Pri:4), Juan Pablo (Pri:3), Veronica (Pri:6), Victoria (Pri:1), Victoria (Pri:6)]
+        // Expected Result: [Andy (Pri:4), Juan Pablo (Pri:3), Victoria (Pri:1)]
+        Console.WriteLine("Test 3");
+        priorityQueue3.Enqueue("Andy", 4);
+        priorityQueue3.Enqueue("Juan Pablo", 3);
+        priorityQueue3.Enqueue("Veronica", 6);
+        priorityQueue3.Enqueue("Victoria", 1);
+         priorityQueue3.Enqueue("Victoria", 6);
+        Console.WriteLine("High priority");
+        Console.WriteLine("Before Dequeue");
+        Console.WriteLine(priorityQueue3);
+        Console.WriteLine("------------------");
+        Console.WriteLine("Dequeued element");
+        Console.WriteLine(priorityQueue3.Dequeue());
+        Console.WriteLine("------------------");
+        Console.WriteLine("Actual Queue");
+        Console.WriteLine(priorityQueue3);
+        priorityQueue3.Dequeue();
 
-        // Add more Test Cases As Needed Below
     }
 }
--- week02/code/PriorityQueue.cs
+++ week02/code/PriorityQueue.cs
@@ -1,5 +1,7 @@
-﻿public class PriorityQueue {
-    private List<PriorityItem> _queue = new();
+﻿using System.Runtime.CompilerServices;
+
+public class PriorityQueue {
+    private Queue<PriorityItem> _queue = new();
 
     /// <summary>
     /// Add a new value to the queue with an associated priority.  The
@@ -10,10 +12,10 @@
     /// <param name="priority">The priority</param>
     public void Enqueue(string value, int priority) {
         var newNode = new PriorityItem(value, priority);
-        _queue.Add(newNode);
+        _queue.Enqueue(newNode);
     }
 
-    public String Dequeue() {
+    public PriorityItem Dequeue() {
         if (_queue.Count == 0) // Verify the queue is not empty
         {
             Console.WriteLine("The queue is empty.");
@@ -21,15 +23,46 @@
         }
 
         // Find the index of the item with the highest priority to remove
-        var highPriorityIndex = 0;
-        for (int index = 1; index < _queue.Count - 1; index++) {
-            if (_queue[index].Priority >= _queue[highPriorityIndex].Priority)
-                highPriorityIndex = index;
+        List<int> indexList = new();
+        foreach (PriorityItem item in _queue){
+            
+            indexList.Add(item.Priority);
+
+        }
+        //Console.WriteLine(indexList);
+         // Inicializar maxNumber con el primer elemento de la lista
+        int maxNumber = indexList[0];
+
+        // Recorrer la lista para encontrar el número mayor
+        foreach (int number in indexList)
+        {
+            if (number <= maxNumber)
+            {
+                continue;
+            }
+            maxNumber = number;
         }
 
+        var itemDequeue = new PriorityItem("kev", 0);
+        List<PriorityItem> newQueu = new List<PriorityItem>();
+        foreach (var element in _queue){
+            
+            if(element.Priority == maxNumber){
+               itemDequeue = element;
+            }
+            else {
+                 newQueu.Add(element);
+            }   
+
+        }
+        Queue<PriorityItem> newQ = new();
+        foreach(PriorityItem item in newQueu){
+            newQ.Enqueue(item);
+        }
+       _queue = newQ;
         // Remove and return the item with the highest priority
-        var value = _queue[highPriorityIndex].Value;
-        return value;
+        //var value = _queue[highPriorityIndex].Value;
+        return itemDequeue;
     }
 
     public override string ToString() {
@@ -37,13 +70,17 @@
     }
 }
 
-internal class PriorityItem {
-    internal string Value { get; set; }
-    internal int Priority { get; set; }
+public class PriorityItem {
+    public string Value { get; set; }
+    public int Priority { get; set; }
 
-    internal PriorityItem(string value, int priority) {
+    public PriorityItem(string value, int priority) {
         Value = value;
         Priority = priority;
+    }
+    public PriorityItem() {
+        Value = "";
+        Priority = 0;
     }
 
     public override string ToString() {
